18e6d3f36ec784790e96ff5f3c98141b66bd0fc0
Pietro Sette <pietrosette@gmail.com>
2020-08-30 18:00:39 +0200

    handlebars + ajax done

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
-$(document).ready(function() {
-	//Code
+$(document).ready(function(){
+
+    $.ajax(
+        {
+            url: "https://flynn.boolean.careers/exercises/api/array/music",
+            method: "GET",
+            success: function (risposta) {
+                // console.log(risposta);
+
+                var source = $("#entry-template").html();
+                var template = Handlebars.compile(source);
+
+                for (var i = 0; i < risposta.response.length; i++){
+                var html = template(risposta.response[i]);
+                console.log(html);
+                $('.cds-container').append(html);
+                };
+            },
+            error: function (richiesta, stato, errori) {
+                alert("E' avvenuto un errore. " + errore);
+            }
+        }
+    );
 });
